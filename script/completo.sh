dd10f70f4e1b94a6a2ae938db171d986de22c3bc
marina <marina@debian>
2025-12-11 10:13:17 +0100

    corregido completo.sh para que se cree un archivo con el nombre de la ultima copia completa

--- script/completo.sh
+++ script/completo.sh
@@ -7,7 +7,7 @@ COPIA="/var /etc /home /root /usr/local /opt /srv /boot"
 DATE=$(date +%F)
 RUTA_LOCAL="/mnt/vg1/full"
 CLAVE="/home/debian/.ssh/backup"
-DESTINO_REMOTO="debian@10.0.0.22:/home/debian/full/backup-$DATE"
+DESTINO_REMOTO="debian@10.0.0.22:/mnt/backup/full/backup-$DATE"
 
 #lo primero será montar la partición
 #mount /dev/vg1/lv-backup /mnt/vg1/
@@ -21,11 +21,19 @@ else
         echo "La partición se ha montado."
 fi
 
-#rsync
-rsync -aAXHzv --delete $COPIA $RUTA_LOCAL/backup-$DATE 
+#rsync para crear backup completo
+rsync -aAXHzv --delete $COPIA $RUTA_LOCAL/backup-$DATE
 #scp -i $CLAVE -r $RUTA_LOCAL/backup-$DATE debian@10.0.0.22:/home/debian/full/backup-$DATE
+#enviar backup al servidor remoto
 rsync -aAXHzv -e "ssh -i $CLAVE -o StrictHostKeyChecking=no" \
     $RUTA_LOCAL/backup-$DATE/ $DESTINO_REMOTO/
 
-#desmontar disco
-#umount /mnt/vg1/
+# Guardar la fecha del último backup completo para los incrementales
+echo "$DATE" > "$RUTA_LOCAL/last_full"
+
+#borrar copias con un mes de anitigüedad
+find "$RUTA_LOCAL" -maxdepth 1 -type d -name "backup-*" -mtime +30 -exec rm -rf {} \;
+
+# borrar copias remotas de más de 30 días
+ssh -i "$CLAVE" debian@10.0.0.22 \
+'find /mnt/backup/full -maxdepth 1 -type d -name "backup-*" -mtime +30 -exec rm -rf {} \;'
